c40d361e53fcfae638c8fe4585fd0ea336a1107b
Mevadashubham <mevadashubham@gmail.com>
2026-01-17 18:35:06 +0530

    Changes

--- CourseMate_Back/server.js
+++ CourseMate_Back/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 const app = express();
 
-app.use(cors());
+/* ----------- MIDDLEWARE ----------- */
+app.use(
+  cors({
+    origin: [
+      "https://course-mate-project-z9wv-bwsui0oug-shubham-mevadas-projects.vercel.app/",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -27,10 +37,13 @@ app.use(roleRoutes);
 const assignmentRoutes = require("./src/routes/assignmentRoutes");
 app.use("/assignments", assignmentRoutes);
 
+/* ----------- DB + SERVER ----------- */
+const PORT = process.env.PORT || 5000;
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/CourseMate_Backend")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => console.log("Server running on port 5000"));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .catch((err) => console.error("MongoDB error:", err));
